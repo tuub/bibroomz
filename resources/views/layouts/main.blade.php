a0ab16262151b008e71224a4758b0ff95a3aa7b2
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2022-09-02 22:52:19 +0200

    Added base views.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>{{ env('APP_NAME') }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="api-base-url" content="{{ url('/') }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="stylesheet" href="{!! url('css/app.css') !!}">
+    <meta name="theme-color" content="#fafafa">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+<div id="app">
+    @yield('content')
+</div>
+@vite('resources/js/app.js')
+</body>
+</html>
