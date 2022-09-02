e4c9b0e002978e5a8a9130aec69128c648bea844
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2022-09-02 23:01:19 +0200

    Added tailwind css.

---
+++ resources/js/App.vue
@@ -0,0 +1,3 @@
+<template>
+    ROOMZ
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import '../css/app.css'
+
+createApp(App).mount('#app')
