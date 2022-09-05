8a2a5d69774ca00709054eefc6856215cab66174
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2022-09-05 17:51:18 +0200

    Updated vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
