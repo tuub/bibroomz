93f383632951c584f1c0092acb42d3ae3bd7b393
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2022-09-08 09:09:43 +0200

    Added base layout.

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-row gap-5">
+        <div id="sidebar" class="basis-1/5 md:basis-1/5">
+            <LoginForm></LoginForm>
+        </div>
+        <div id="calendar" class="basis-4/5 md:basis-4/5">
+            <Calendar></Calendar>
+        </div>
+    </div>
+</template>
+
+<script>
+import Calendar from "../Components/Calendar.vue";
+import LoginForm from "../Components/LoginForm.vue";
+
+export default {
+    components: {
+        Calendar,
+        LoginForm,
+    },
+}
+
+
+</script>
+
+<style scoped>
+#sidebar {
+    background-color: #ffffff;
+}
+</style>
