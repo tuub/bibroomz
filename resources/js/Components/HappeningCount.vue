e3fd8d79b8ea80a316d57d37cda06a56633a9803
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2023-05-03 13:18:46 +0200

    [#13] Show count of reservations in user happenings list

---
+++ resources/js/Components/HappeningCount.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-gray-200 p-2">
+        <p class="italic">{{ count }} reservations</p>
+    </div>
+</template>
+
+<script setup>
+let props = defineProps({
+    count: Number,
+});
+</script>
--- resources/js/Components/UserHappenings.vue
+++ resources/js/Components/UserHappenings.vue
@@ -3,30 +3,34 @@
     <div v-if="happeningsCount === 0" class="text-sm mt-5">
         You have no current reservations. Add some!
     </div>
-    <ul class="list-decimal mt-5 ml-5" v-if="happenings">
-        <!--
-        <TransitionGroup name="list"
-                         tag="ul"
-                         enter-from-class="opacity-0 scale-125"
-                         enter-to-class="opacity-100 scale-100"
-                         enter-active-class="transition duration-300"
-                         leave-from-class="opacity-100 scale-100"
-                         leave-to-class="opacity-0 scale-125"
-                         leave-active-class="transition duration-200"
-        >
-        -->
-        <TransitionGroup name="list" tag="ul">
-            <li v-for="happening in happenings" :key="happening.id" class="mb-2">
-                <UserHappening :happening="happening"></UserHappening>
-            </li>
-        </TransitionGroup>
-    </ul>
+    <div v-if="happenings">
+        <HappeningCount :count="happeningsCount"></HappeningCount>
+        <ul class="list-decimal mt-5 ml-5" v-if="happenings">
+            <!--
+            <TransitionGroup name="list"
+                             tag="ul"
+                             enter-from-class="opacity-0 scale-125"
+                             enter-to-class="opacity-100 scale-100"
+                             enter-active-class="transition duration-300"
+                             leave-from-class="opacity-100 scale-100"
+                             leave-to-class="opacity-0 scale-125"
+                             leave-active-class="transition duration-200"
+            >
+            -->
+            <TransitionGroup name="list" tag="ul">
+                <li v-for="happening in happenings" :key="happening.id" class="mb-2">
+                    <UserHappening :happening="happening"></UserHappening>
+                </li>
+            </TransitionGroup>
+        </ul>
+    </div>
 </template>
 
 <script setup>
 
 import UserHappening from './UserHappening.vue'
 import {computed} from "vue";
+import HappeningCount from "./HappeningCount.vue";
 
 let props = defineProps({
     happenings: Object,
