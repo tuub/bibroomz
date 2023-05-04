71fec713f38854b9e2efe3d6825fde91e5dc2c53
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2023-05-04 10:52:09 +0200

    [#14] Use ordered list in User Happenings

--- resources/js/Components/UserHappenings.vue
+++ resources/js/Components/UserHappenings.vue
@@ -5,24 +5,22 @@
     </div>
     <div v-if="happenings">
         <HappeningCount :count="happeningsCount"></HappeningCount>
-        <ul class="list-decimal mt-5 ml-5" v-if="happenings">
-            <!--
-            <TransitionGroup name="list"
-                             tag="ul"
-                             enter-from-class="opacity-0 scale-125"
-                             enter-to-class="opacity-100 scale-100"
-                             enter-active-class="transition duration-300"
-                             leave-from-class="opacity-100 scale-100"
-                             leave-to-class="opacity-0 scale-125"
-                             leave-active-class="transition duration-200"
-            >
-            -->
-            <TransitionGroup name="list" tag="ul">
-                <li v-for="happening in happenings" :key="happening.id" class="mb-2">
-                    <UserHappening :happening="happening"></UserHappening>
-                </li>
-            </TransitionGroup>
-        </ul>
+        <!--
+        <TransitionGroup name="list"
+                         tag="ul"
+                         enter-from-class="opacity-0 scale-125"
+                         enter-to-class="opacity-100 scale-100"
+                         enter-active-class="transition duration-300"
+                         leave-from-class="opacity-100 scale-100"
+                         leave-to-class="opacity-0 scale-125"
+                         leave-active-class="transition duration-200"
+        >
+        -->
+        <TransitionGroup name="list" tag="ol" class="list-decimal mt-5 ml-5">
+            <li v-for="happening in happenings" :key="happening.id" class="mb-2">
+                <UserHappening :happening="happening"></UserHappening>
+            </li>
+        </TransitionGroup>
     </div>
 </template>
 
