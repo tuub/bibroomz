5ef523e1a225628c757eb0fe236144e026dd1e89
schalljugend <fabian.fuerste@tu-berlin.de>
2023-07-20 15:53:26 +0200

    [FIX] Fixed import of dayjs in AuthStore

--- resources/js/Stores/AuthStore.js
+++ resources/js/Stores/AuthStore.js
@@ -1,4 +1,4 @@
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import { defineStore } from 'pinia';
 import { useToast } from 'vue-toastification';
 import { router } from '@inertiajs/vue3'
