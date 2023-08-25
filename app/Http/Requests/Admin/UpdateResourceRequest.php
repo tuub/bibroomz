cd0d0132e75559fc4dfd93c218f2ab6db45186a7
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2023-08-25 12:40:00 +0200

    [#138] Location URI with validation rule "url" but nullable

--- app/Http/Requests/Admin/UpdateResourceRequest.php
+++ app/Http/Requests/Admin/UpdateResourceRequest.php
@@ -27,7 +27,7 @@ class UpdateResourceRequest extends FormRequest
             'institution_id' => ['required'],
             'title' => ['required'],
             'location' => ['required'],
-            'location_uri' => ['url'],
+            'location_uri' => ['url', 'nullable'],
             'description' => ['required'],
             'capacity' => ['numeric', 'gt:0'],
             'is_active' => ['required'],
