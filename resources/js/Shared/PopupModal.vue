86afa6d707f8821a5266eac469fac2a19a3e4b19
Max-Georg Debbert <m.debbert@tu-berlin.de>
2023-09-09 20:43:13 +0200

    [#185] Fix delete modal

--- resources/js/Shared/PopupModal.vue
+++ resources/js/Shared/PopupModal.vue
@@ -14,9 +14,8 @@
             class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
         >
             <div class="relative w-full max-w-md max-h-full">
-                <div
-                    class="relative bg-white rounded-lg shadow dark:bg-gray-700"
-                >
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <!-- Close button -->
                     <button
                         type="button"
                         class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -39,6 +38,8 @@
                         </svg>
                         <span class="sr-only">Close modal</span>
                     </button>
+
+                    <!-- Content -->
                     <div class="p-6 text-center">
                         <svg
                             class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
@@ -55,27 +56,24 @@
                                 d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                             />
                         </svg>
-                        <h3
-                            class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400"
-                        >
+
+                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
                             {{ modal.content?.message }}
                         </h3>
-                        <!-- Error alert -->
-                        <ModalAlert
-                            v-if="modal.error"
-                            :error="modal.error"
-                            @close="modal.error = null"
-                        />
+
                         <div>
+                            <!-- Action buttons -->
                             <button
                                 v-for="action in actions"
                                 :key="action.label"
                                 type="button"
                                 class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-                                @click="modal.do(action)"
+                                @click="execute(action)"
                             >
                                 {{ action.label }}
                             </button>
+
+                            <!-- Cancel button -->
                             <button
                                 type="button"
                                 class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
@@ -92,11 +90,18 @@
 </template>
 
 <script setup>
-import ModalAlert from "@/Components/Modals/ModalAlert.vue";
+// IMPORTS
 import useModal from "@/Stores/Modal";
+
 import { storeToRefs } from "pinia";
 
+// VARIABLES
 const modal = useModal();
-
 const { actions } = storeToRefs(modal);
+
+// METHODS
+const execute = (action) => {
+    action.callback(modal.payload);
+    modal.close();
+};
 </script>
