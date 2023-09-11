a1142122300a0416ccdd05cc04d85b0f04bfba42
Max-Georg Debbert <m.debbert@tu-berlin.de>
2023-09-11 13:58:12 +0200

    [#189] Add autocomplete attributes to login form

--- resources/js/Components/Modals/LoginModal.vue
+++ resources/js/Components/Modals/LoginModal.vue
@@ -19,6 +19,7 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     name="username"
+                    autocomplete="username"
                     :placeholder="$t('login.form.username.placeholder')"
                 />
 
@@ -40,6 +41,7 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="password"
                     name="password"
+                    autocomplete="current-password"
                     :placeholder="$t('login.form.password.placeholder')"
                 />
 
