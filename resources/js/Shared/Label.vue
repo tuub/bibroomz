dd9863712253eec2fff1ac8ee4e3a412ad2eb810
Max-Georg Debbert <m.debbert@tu-berlin.de>
2023-09-26 10:35:51 +0200

    [#228] Fix eslint warnings in Label.vue component

--- resources/js/Shared/Label.vue
+++ resources/js/Shared/Label.vue
@@ -1,44 +1,43 @@
 <template>
-    <span id="label"
-          class="text-xs font-semibold inline-block py-2 px-3 uppercase rounded uppercase last:mr-0 mr-1 mt-1"
-          :class="[classTextColor, classBgColor]"
-          :style="{'color': styleTextColor, 'background-color': styleBgColor}">
-        <span :class="iconClass" class="mr-1" v-if="iconClass"></span>
+    <span
+        id="label"
+        class="text-xs font-semibold inline-block py-2 px-3 rounded uppercase last:mr-0 mr-1 mt-1"
+        :class="[textColorClass, bgColorClass]"
+        :style="{ color: textColor, 'background-color': bgColor }"
+    >
+        <span v-if="iconClass" :class="iconClass" class="mr-1"></span>
         {{ text }}
     </span>
 </template>
 
 <script setup>
-import {computed} from "vue";
-
 // ------------------------------------------------
 // Props
 // ------------------------------------------------
-const props = defineProps({
-    text: String,
-    bgColor: String,
-    bgColorClass: String,
-    textColor: String,
-    textColorClass: String,
-    iconClass: String,
-})
-
-// ------------------------------------------------
-// Variables
-// ------------------------------------------------
-const classTextColor = computed(() => {
-    return props.textColorClass !== 'undefined' ? props.textColorClass : ''
-})
-
-const classBgColor = computed(() => {
-    return props.bgColorClass !== 'undefined' ? props.bgColorClass : ''
-})
-
-const styleTextColor = computed(() => {
-    return props.textColor !== undefined ? props.textColor : ''
-})
-
-const styleBgColor = computed(() => {
-    return props.bgColor !== undefined ? props.bgColor : ''
-})
+defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+    bgColor: {
+        type: String,
+        default: "",
+    },
+    bgColorClass: {
+        type: String,
+        default: "",
+    },
+    textColor: {
+        type: String,
+        default: "",
+    },
+    textColorClass: {
+        type: String,
+        default: "",
+    },
+    iconClass: {
+        type: String,
+        required: true,
+    },
+});
 </script>
