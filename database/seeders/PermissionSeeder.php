69442dd5b7e225762b4ce0ed04261f97469cd8c3
Max-Georg Debbert <m.debbert@tu-berlin.de>
2023-09-26 19:43:02 +0200

    [#223] Seed German names in permissions seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -5,9 +5,74 @@ namespace Database\Seeders;
 use App\Models\Permission;
 use App\Models\PermissionGroup;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class PermissionSeeder extends Seeder
 {
+    private Collection $verbs;
+    private Collection $groups;
+
+    public function __construct()
+    {
+        $this->verbs = collect([
+            'view' => [
+                'en' => 'view',
+                'de' => 'anzeigen',
+            ],
+            'create' => [
+                'en' => 'create',
+                'de' => 'erstellen',
+            ],
+            'edit' => [
+                'en' => 'edit',
+                'de' => 'bearbeiten',
+            ],
+            'delete' => [
+                'en' => 'delete',
+                'de' => 'löschen',
+            ],
+        ]);
+
+        $this->groups = collect([
+            'institutions' => [
+                'en' => 'Institutions',
+                'de' => 'Einrichtungen',
+            ],
+            'resources' => [
+                'en' => 'Rooms',
+                'de' => 'Räume',
+            ],
+            'closings' => [
+                'en' => 'Closings',
+                'de' => 'Schließungen',
+            ],
+            'happenings' => [
+                'en' => 'Bookings',
+                'de' => 'Buchungen',
+            ],
+            'users' => [
+                'en' => 'Users',
+                'de' => 'Benutzer',
+            ],
+            'roles' => [
+                'en' => 'Roles',
+                'de' => 'Rollen',
+            ],
+            'institution' => [
+                'en' => 'Institution',
+                'de' => 'Einrichtung',
+            ],
+            'admin_users' => [
+                'en' => 'Administrators',
+                'de' => 'Administratoren',
+            ],
+            'mails' => [
+                'en' => 'Mails',
+                'de' => 'Mails',
+            ],
+        ]);
+    }
+
     /**
      * Run the database seeds.
      *
@@ -15,14 +80,16 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        $verbs = [
+        foreach ($this->groups as $key => $name) {
+            $this->createPermissionGroup($key, $name);
+        }
+
+        $this->createPermissions([
             'view',
             'create',
             'edit',
             'delete',
-        ];
-
-        $models = [
+        ], [
             'institutions',
             'resources',
             'closings',
@@ -30,58 +97,67 @@ class PermissionSeeder extends Seeder
             'users',
             'roles',
             'mails',
-        ];
+        ]);
 
-        foreach ($models as $model) {
-            $this->createPermissionGroup($model);
-            $this->createPermissions($verbs, $model);
-        }
+        $this->createPermissions([
+            'view',
+            'edit',
+            'delete',
+        ], [
+            'institution',
+        ]);
 
-        $this->createPermissions(['view', 'edit', 'delete'], 'institution');
-        $this->createPermissions(['view', 'edit', 'delete'], 'admin users');
+        $this->createPermissions([
+            'view',
+            'edit',
+            'delete',
+        ], [
+            'admin_users',
+        ]);
 
         Permission::create([
             'key' => 'unlimited_quotas',
             'name' => [
                 'en' => 'Unlimited quotas',
+                'de' => 'Unbegrenzte Kontingente',
             ],
         ]);
 
         Permission::create([
             'key' => 'no_verifier',
             'name' => [
-                'en' => 'No verifier',
+                'en' => 'No verification necessary',
+                'de' => 'Keine Bestätigung notwendig',
             ],
         ]);
     }
 
-    private function createPermissions(array $verbs, string $model)
+    private function createPermissions(array $verbs, array $groups)
     {
-        foreach ($verbs as $verb) {
-            $this->createPermission($verb, $model);
+        foreach ($verbs as $verbKey) {
+            foreach ($groups as $groupKey) {
+                $verbName = $this->verbs->get($verbKey);
+                $groupName = $this->groups->get($groupKey);
+
+                $permission = Permission::create([
+                    'key' => $verbKey . '_' . $groupKey,
+                    'name' => [
+                        'en' => ucfirst($verbName['en']) . ' ' . lcfirst($groupName['en']),
+                        'de' => ucfirst($groupName['de']) . ' ' . lcfirst($verbName['de']),
+                    ],
+                ]);
+
+                $group = PermissionGroup::where('key', '=', $groupKey)->first();
+                $permission->group()->associate($group)->save();
+            }
         }
     }
 
-    private function createPermission(String $verb, String $model)
-    {
-        $permission = Permission::create([
-            'key' => implode('_', [$verb, str_replace(' ', '_', $model)]),
-            'name' => [
-                'en' => implode(' ', [ucfirst($verb), $model]),
-            ],
-        ]);
-
-        $group = PermissionGroup::where('key', '=', $model)->first();
-        $permission->group()->associate($group)->save();
-    }
-
-    private function createPermissionGroup(String $model)
+    private function createPermissionGroup(string $key, array $name)
     {
         PermissionGroup::create([
-            'key' => $model,
-            'name' => [
-                'en' => ucfirst($model),
-            ],
+            'key' => $key,
+            'name' => $name,
         ]);
     }
 }
