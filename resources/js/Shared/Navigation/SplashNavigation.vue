bed2894899a43c309670920ff0dd2913fff56256
Max-Georg Debbert <m.debbert@tu-berlin.de>
2024-01-09 10:38:46 +0100

    [#397] Link help page in menu on start page

--- resources/js/Shared/Navigation/SplashNavigation.vue
+++ resources/js/Shared/Navigation/SplashNavigation.vue
@@ -1,17 +1,21 @@
 <template>
-    <NavigationMenu v-if="isPrivileged">
+    <NavigationMenu>
         <ul>
-            <NavLink icon="ri-tools-fill" :href="route('admin.dashboard')">
+            <NavLink v-if="isPrivileged" icon="ri-tools-fill" :href="route('admin.dashboard')">
                 <li>
                     {{ $t("navigation.regular.admin") }}
                 </li>
             </NavLink>
+            <ExternalLink icon="ri-question-fill" :href="$t('navigation.regular.help.uri')">
+                <li>{{ $t("navigation.regular.help.text") }}</li>
+            </ExternalLink>
         </ul>
     </NavigationMenu>
 </template>
 
 <script setup>
 import NavLink from "@/Shared/NavLink.vue";
+import ExternalLink from "@/Shared/Navigation/ExternalLink.vue";
 import NavigationMenu from "@/Shared/Navigation/NavigationMenu.vue";
 import { useAuthStore } from "@/Stores/AuthStore";
 
