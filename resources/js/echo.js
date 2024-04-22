40460cf18c72ebeaea2ef8f4b22c10736cbe9c01
Max-Georg Debbert <m.debbert@tu-berlin.de>
2024-04-22 11:45:52 +0200

    [#433] Make echo broadcast driver configurable via environment variable

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,7 +4,7 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
+    broadcaster: import.meta.env.VITE_BROADCAST_DRIVER ?? 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY ?? import.meta.env.VITE_PUSHER_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST ?? import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? import.meta.env.VITE_PUSHER_PORT ?? 80,
