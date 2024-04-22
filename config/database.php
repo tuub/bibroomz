8abb4ce3c897a9cb764e8a72303e7891c6954775
Max-Georg Debbert <m.debbert@tu-berlin.de>
2024-04-22 10:29:11 +0200

    [#431] Use mysql database driver

--- config/database.php
+++ config/database.php
@@ -43,8 +43,8 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mariadb' => [
-            'driver' => 'mariadb',
+        'mysql' => [
+            'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
