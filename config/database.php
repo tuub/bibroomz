2b97ebe018f2ee22752156733cf09023be6ee532
Max-Georg Debbert <m.debbert@tu-berlin.de>
2024-04-22 12:02:05 +0200

    [#434] Fix default database connection

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mariadb'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
