9979d6ca8d308db949220ac69a23d946335d713e
Max-Georg Debbert <m.debbert@tu-berlin.de>
2024-06-17 16:38:06 +0200

    fix: decrypt locale cookie before reading it
    closes #443

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\Localization::class,
     ];
 
     /**
@@ -39,6 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \App\Http\Middleware\CacheUserActivity::class,
+            \App\Http\Middleware\Localization::class,
         ],
 
         'api' => [
