640cbf85a1bc2b55ed986c1692c35447ff03593a
Max-Georg Debbert <m.debbert@tu-berlin.de>
2024-06-26 10:51:09 +0200

    style: fix formatting

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,14 +1,14 @@
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
-import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: import.meta.env.VITE_BROADCAST_DRIVER ?? 'reverb',
+    broadcaster: import.meta.env.VITE_BROADCAST_DRIVER ?? "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY ?? import.meta.env.VITE_PUSHER_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST ?? import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+    enabledTransports: ["ws", "wss"],
 });
