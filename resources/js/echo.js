34f64b0201a8a538d21a188f0d96ff6cc9d2b553
Max-Georg Debbert <m.debbert@tu-berlin.de>
2024-07-08 16:21:15 +0200

    refactor(echo.js): extract base url

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,28 +1,17 @@
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 
+const baseUrl = new URL(import.meta.env.VITE_API_URL);
+const protocol = baseUrl.protocol.replace(":", "");
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: import.meta.env.VITE_BROADCAST_DRIVER ?? "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY ?? import.meta.env.VITE_PUSHER_APP_KEY ?? "bibroomz",
-    wsHost:
-        import.meta.env.VITE_REVERB_HOST ??
-        import.meta.env.VITE_PUSHER_HOST ??
-        new URL(import.meta.env.VITE_API_URL).hostname,
-    wsPort:
-        import.meta.env.VITE_REVERB_PORT ??
-        import.meta.env.VITE_PUSHER_PORT ??
-        new URL(import.meta.env.VITE_API_URL).port ??
-        80,
-    wssPort:
-        import.meta.env.VITE_REVERB_PORT ??
-        import.meta.env.VITE_PUSHER_PORT ??
-        new URL(import.meta.env.VITE_API_URL).port ??
-        443,
-    forceTLS:
-        (import.meta.env.VITE_REVERB_SCHEME ??
-            import.meta.env.VITE_PUSHER_SCHEME ??
-            new URL(import.meta.env.VITE_API_URL).protocol.replace(":", "")) === "https",
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? import.meta.env.VITE_PUSHER_HOST ?? baseUrl.hostname,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? import.meta.env.VITE_PUSHER_PORT ?? baseUrl.port ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? import.meta.env.VITE_PUSHER_PORT ?? baseUrl.port ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? import.meta.env.VITE_PUSHER_SCHEME ?? protocol) === "https",
     enabledTransports: ["ws", "wss"],
 });
