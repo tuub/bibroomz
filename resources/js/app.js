67ab525383ff030d8f6dab95dc0aadd409ef0e88
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2024-07-09 10:34:03 +0200

    refactor: reorder vue app setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,34 @@
 import MainLayout from "@/Layouts/MainLayout.vue";
 
 import "./bootstrap";
-import { Ziggy } from "./ziggy";
 
+import { createApp, h } from "vue";
 import { Head, Link, createInertiaApp } from "@inertiajs/vue3";
-import Aura from "@primevue/themes/aura";
+
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { i18nVue } from "laravel-vue-i18n";
+
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import PrimeVue from "primevue/config";
-import "remixicon/fonts/remixicon.css";
-import { createApp, h } from "vue";
-import Toast from "vue-toastification";
-import "vue-toastification/dist/index.css";
+
+import { Ziggy } from "./ziggy";
 import { ZiggyVue, route } from "ziggy-js";
+
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import ToastService from "primevue/toastservice";
+import DialogService from "primevue/dialogservice";
+import ConfirmationService from "primevue/confirmationservice";
+import BadgeDirective from "primevue/badgedirective";
+import Ripple from "primevue/ripple";
+import StyleClass from "primevue/styleclass";
+import ConfirmDialog from "primevue/confirmdialog";
+import ConfirmPopup from "primevue/confirmpopup";
+import Dialog from "primevue/dialog";
+import Toast from "primevue/toast";
+import Tooltip from "primevue/tooltip";
+
+import "remixicon/fonts/remixicon.css";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
@@ -46,30 +60,46 @@ createInertiaApp({
 
         const app = createApp({ render: () => h(App, props) });
 
-        app.use(PrimeVue, {
-            theme: {
-                preset: Aura,
-                options: {
-                    darkModeSelector: "system",
-                    cssLayer: {
-                        name: "primevue",
-                        order: "tailwind-base, primevue, tailwind-utilities",
+        app.use(plugin)
+            // PrimeVue
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura,
+                    options: {
+                        darkModeSelector: "system",
+                        cssLayer: {
+                            name: "primevue",
+                            order: "tailwind-base, primevue, tailwind-utilities",
+                        },
+                        ripple: true,
                     },
-                    ripple: true,
                 },
-            },
-        });
-
-        app.use(pinia)
-            .component("Head", Head)
-            .component("Link", Link)
-            .use(plugin)
+            })
+            .use(ToastService)
+            .use(DialogService)
+            .use(ConfirmationService)
+            .directive("tooltip", Tooltip)
+            .directive("badge", BadgeDirective)
+            .directive("ripple", Ripple)
+            .directive("styleclass", StyleClass)
+            .component("Toast", Toast)
+            .component("ConfirmDialog", ConfirmDialog)
+            .component("ConfirmPopup", ConfirmPopup)
+            .component("Dialog", Dialog)
+            // Pinia
+            .use(pinia)
+            // i18n
             .use(i18nVue, {
                 lang: "de",
                 fallback: "en",
                 resolve: (lang) => import(`../../lang/php_${lang}.json`),
             })
+            // Ziggy
             .use(ZiggyVue, Ziggy)
+            .provide("ziggyRoute", (name, params, absolute, config = Ziggy) => {
+                return route(name, params, absolute, config);
+            })
+            // Toast
             .use(Toast, {
                 maxToasts: 10,
                 newestOnTop: true,
@@ -92,11 +122,10 @@ createInertiaApp({
 
                     return toast;
                 },
-            });
-
-        app.provide("ziggyRoute", (name, params, absolute, config = Ziggy) => {
-            return route(name, params, absolute, config);
-        });
+            })
+            // Custom components
+            .component("Head", Head)
+            .component("Link", Link);
 
         app.mount(el);
     },
