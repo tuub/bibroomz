ae788ded4cdf31a030c1a8a0378155deae7f4be8
Max-Georg Debbert <m.debbert@tu-berlin.de>
2025-04-08 14:02:01 +0200

    ci: add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+prefix="$1"
+
+composer install
+npm clean-install
+
+php artisan config:cache
+php artisan route:cache
+
+php artisan migrate --force --seed
+
+php artisan ziggy:generate
+
+npm run build --base="$prefix/build"
