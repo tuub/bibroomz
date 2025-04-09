be89183d046cf604be4c1b914d494e5564f4226e
Max-Georg Debbert <m.debbert@tu-berlin.de>
2025-04-09 11:15:47 +0200

    build: don't cache routes in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,9 @@ prefix="$1"
 composer install
 npm clean-install
 
-php artisan config:cache
-php artisan route:cache
+php artisan optimize --except=routes
+php artisan ziggy:generate
 
 php artisan migrate --force --seed
-
-php artisan ziggy:generate
 
 npm run build -- --base="$prefix/build"
