4ebc773bd496ddfc235f334aa2daaa1ea4093859
Max-Georg Debbert <m.debbert@tu-berlin.de>
2025-11-19 11:49:09 +0100

    style(devenv): format flake

--- flake.nix
+++ flake.nix
@@ -4,25 +4,35 @@
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, ... }@inputs:
-    let forEachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
-    in {
+  outputs =
+    { self, ... }@inputs:
+    let
+      forEachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
+    in
+    {
       inherit (inputs) nixpkgs;
 
-      packages = forEachSystem (system: { devenv-up = self.devShells.${system}.default.config.procfileScript; });
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
 
-      devShells = forEachSystem (system:
-        let pkgs = import inputs.nixpkgs { inherit system; };
-        in {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+        in
+        {
           default = inputs.devenv.lib.mkShell {
             inherit inputs pkgs;
 
             modules = [ ./devenv.nix ];
           };
-        });
+        }
+      );
     };
 }
