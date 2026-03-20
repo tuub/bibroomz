15f5ad68aa1979863c23a837ee3850263ec46011
Fab Fuerste <fabian.fuerste@tu-berlin.de>
2026-03-20 13:10:04 +0100

    refactor(admin): add index page for mails

--- resources/js/Pages/Admin/Mails/Index.vue
+++ resources/js/Pages/Admin/Mails/Index.vue
@@ -1,74 +1,20 @@
-<template>
-    <IndexLayout
-        model="mail"
-        :title="$t('admin.mails.index.title', { title: translate(institution.title) })"
-        :description="$t('admin.mails.index.description')"
-        :create-params="{ institution_id: institution.id }"
-    >
-        <template #header>
-            <IndexHeaderField>
-                {{ $t("admin.mails.index.table.header.mail_type") }}
-            </IndexHeaderField>
-            <IndexHeaderField>
-                {{ $t("admin.mails.index.table.header.subject") }}
-            </IndexHeaderField>
-            <IndexHeaderField class="text-center">
-                {{ $t("admin.mails.index.table.header.is_active") }}
-            </IndexHeaderField>
-            <IndexHeaderField :is-label-visible="false">
-                {{ $t("admin.general.table.actions") }}
-            </IndexHeaderField>
-        </template>
-
-        <template #body>
-            <IndexRow v-for="mail in mails" :key="mail.id">
-                <IndexRowHeaderField>
-                    {{ $t("admin.mails.mail_types." + mail.mail_type.key) }}
-                </IndexRowHeaderField>
-                <IndexRowField class="align-top">
-                    {{ translate(mail.subject) }}
-                </IndexRowField>
-                <IndexRowField class="text-center align-top">
-                    <BooleanField :is-true="mail.is_active" />
-                </IndexRowField>
-                <IndexRowField class="text-right align-top">
-                    <LinkGroup>
-                        <ActionLink
-                            v-if="hasPermission('edit_mails', mail.institution_id)"
-                            action="edit"
-                            model="mail"
-                            :params="{ id: mail.id }"
-                        />
-                        <PopupLink
-                            v-if="hasPermission('delete_mails', mail.institution_id)"
-                            action="delete"
-                            model="mail"
-                            :params="{ id: mail.id }"
-                        />
-                    </LinkGroup>
-                </IndexRowField>
-            </IndexRow>
-        </template>
-    </IndexLayout>
-</template>
-
 <script setup>
 import ActionLink from "@/Components/Admin/Index/ActionLink.vue";
 import BooleanField from "@/Components/Admin/Index/BooleanField.vue";
+import CreateLink from "@/Components/Admin/Index/CreateLink.vue";
 import LinkGroup from "@/Components/Admin/Index/LinkGroup.vue";
 import PopupLink from "@/Components/Admin/Index/PopupLink.vue";
-import IndexHeaderField from "@/Components/Admin/IndexHeaderField.vue";
-import IndexLayout from "@/Components/Admin/IndexLayout.vue";
-import IndexRow from "@/Components/Admin/IndexRow.vue";
-import IndexRowField from "@/Components/Admin/IndexRowField.vue";
-import IndexRowHeaderField from "@/Components/Admin/IndexRowHeaderField.vue";
 import { useAppStore } from "@/Stores/AppStore";
 import { useAuthStore } from "@/Stores/AuthStore";
+
+import { FilterMatchMode } from "@primevue/core/api";
+import { transChoice } from "laravel-vue-i18n";
+import { computed, ref } from "vue";
 
 // ------------------------------------------------
 // Props
 // ------------------------------------------------
-defineProps({
+const props = defineProps({
     institution: {
         type: Object,
         default: () => ({}),
@@ -88,6 +34,96 @@ const appStore = useAppStore();
 // ------------------------------------------------
 // Variables
 // ------------------------------------------------
-const translate = appStore.translate;
 const { hasPermission } = authStore;
+const { translate } = appStore;
+const indexTable = ref({});
+
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+
+const recordsCount = computed(() => {
+    return indexTable.value.processedData ? indexTable.value.processedData.length : props.mails.length;
+});
+
+// ------------------------------------------------
+// Methods
+// ------------------------------------------------
 </script>
+
+<template>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <DataTable
+            ref="indexTable"
+            v-model:filters="filters"
+            :value="mails"
+            size="medium"
+            striped-rows
+            show-gridlines
+            removable-sort
+            table-style="min-width: 50rem"
+            class="w-full text-left text-sm text-gray-500 dark:text-gray-400"
+        >
+            <template #header>
+                <div class="flex flex-wrap items-center justify-between gap-2">
+                    <div>
+                        <div class="text-xl font-bold">
+                            {{ $t("admin.mails.index.title", { title: translate(institution.title) }) }}
+                        </div>
+                        <div class="italic">{{ $t("admin.mails.index.description") }}</div>
+                    </div>
+                    <div class="flex flex-wrap justify-between gap-2">
+                        <IconField>
+                            <InputIcon>
+                                <i class="pi pi-search" />
+                            </InputIcon>
+                            <InputText
+                                v-model="filters['global'].value"
+                                :placeholder="$t('admin.general.table.keyword_search')"
+                            />
+                        </IconField>
+                        <CreateLink model="mail" />
+                    </div>
+                </div>
+                <div class="mt-2 text-right text-xs">
+                    {{ transChoice("admin.general.records_count", recordsCount, { count: recordsCount }) }}
+                </div>
+            </template>
+            <template #empty>{{ $t("admin.general.table.no_records") }}</template>
+            <template #loading>{{ $t("admin.general.table.loading_records") }}</template>
+            <Column field="mail_type" :sortable="true" :header="$t('admin.mails.index.table.header.mail_type')">
+                <template #body="slotProps">
+                    {{ $t("admin.mails.mail_types." + slotProps.data.mail_type.key) }}
+                </template>
+            </Column>
+            <Column
+                :field="(m) => translate(m.subject)"
+                :sortable="true"
+                :header="$t('admin.mails.index.table.header.subject')"
+            />
+            <Column field="is_active" :sortable="true" :header="$t('admin.mails.index.table.header.is_active')">
+                <template #body="slotProps">
+                    <BooleanField :is-true="slotProps.data.is_active" />
+                </template>
+            </Column>
+            <Column :header="$t('admin.general.table.actions')">
+                <template #body="slotProps">
+                    <LinkGroup>
+                        <ActionLink
+                            v-if="hasPermission('edit_mail', slotProps.data.id)"
+                            action="edit"
+                            model="mail"
+                            :params="{ id: slotProps.data.id }"
+                        />
+                        <PopupLink
+                            v-if="hasPermission('delete_mail', slotProps.data.id)"
+                            action="delete"
+                            model="mail"
+                            :params="{ id: slotProps.data.id }"
+                        />
+                    </LinkGroup>
+                </template>
+            </Column>
+        </DataTable>
+    </div>
+</template>
