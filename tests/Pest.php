6bc0f0522914fb257a6ccd888d70e4338664fca6
Max-Georg Debbert <m.debbert@tu-berlin.de>
2026-09-02 08:53:51 +0200

    test: increase browser test timeout

--- tests/Pest.php
+++ tests/Pest.php
@@ -8,6 +8,8 @@ use Tests\TestCase;
 uses(TestCase::class)->in('Feature', 'Unit');
 uses(BrowserTestCase::class)->in('Browser');
 
+pest()->browser()->timeout(15_000);
+
 require_once __DIR__.'/Helpers.php';
 require_once __DIR__.'/Browser/Support.php';
 require_once __DIR__.'/NamespaceOverrides.php';
