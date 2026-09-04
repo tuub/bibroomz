9bd2481da4c1b3cbc31178e5b50db878ad1f6e34
Max-Georg Debbert <m.debbert@tu-berlin.de>
2026-09-04 12:35:35 +0200

    chore(devshell): enable php-spx for local profiling

--- flake.nix
+++ flake.nix
@@ -24,12 +24,18 @@
               ++ [
                 all.pcov
                 all.redis
+                all.spx
               ];
             extraConfig = ''
               memory_limit=-1
 
               pcov.enabled=1
               pcov.directory=app/
+
+              spx.http_enabled=1
+              spx.http_key=dev
+              spx.http_ip_whitelist=127.0.0.1,::1
+              spx.http_ui_assets_dir=${pkgs.php83.extensions.spx}/share/misc/php-spx/assets/web-ui
             '';
           };
         in
