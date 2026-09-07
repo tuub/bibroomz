b8a6b21bd5887ab1ef4938bd42273da5f017bb38
Max-Georg Debbert <m.debbert@tu-berlin.de>
2026-09-07 13:41:34 +0200

    test: only raise browser test timeout in CI

--- tests/Pest.php
+++ tests/Pest.php
@@ -8,7 +8,9 @@ use Tests\TestCase;
 uses(TestCase::class)->in('Feature', 'Unit');
 uses(BrowserTestCase::class)->in('Browser');
 
-pest()->browser()->timeout(15_000);
+if (getenv('CI')) {
+    pest()->browser()->timeout(60_000);
+}
 
 require_once __DIR__.'/Helpers.php';
 require_once __DIR__.'/Browser/Support.php';
